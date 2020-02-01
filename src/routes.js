96e1d08542afb56a8b31ce3ea17c0bd1309a0028
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-02-01 20:25:15 -0300

    add UserController and store method

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,8 @@
 import { Router } from 'express';
-import { UniqueConstraintError } from 'sequelize';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  try {
-    const user = await User.create({
-      name: 'Emanuel',
-      email: 'teste@teste.com',
-      password_hash: 'asdasd',
-    });
-    res.json(user);
-  } catch (error) {
-    if (error instanceof UniqueConstraintError) {
-      return res.status(400).send('Usuário já cadastrado.');
-    }
-    throw error;
-  }
-});
+routes.post('/users', UserController.store);
 
 export default routes;
