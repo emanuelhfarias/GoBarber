d8ff4260671fdc370b6b4f1e0dd99bf7a3eb0a46
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-02-01 18:21:40 -0300

    add user model

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,23 @@
 import { Router } from 'express';
+import { UniqueConstraintError } from 'sequelize';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'World RocketSeat' });
+routes.get('/', async (req, res) => {
+  try {
+    const user = await User.create({
+      name: 'Emanuel',
+      email: 'teste@teste.com',
+      password_hash: 'asdasd',
+    });
+    res.json(user);
+  } catch (error) {
+    if (error instanceof UniqueConstraintError) {
+      return res.status(400).send('Usuário já cadastrado.');
+    }
+    throw error;
+  }
 });
 
 export default routes;
