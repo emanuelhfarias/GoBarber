0efc3c53145a5e864e4121dfb580b6c4469be80f
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-02-04 23:37:09 -0300

    envia email via nodemailer e recebe via Mailtrap;

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '548179b1e88378',
+    pass: '4b5a801a4edc50',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
