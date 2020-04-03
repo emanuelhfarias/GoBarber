44bb69a3472747f02076cf3d807c045a0fa2c677
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-04-02 22:57:01 -0300

    validaçoes de formulario com Yup
    $ yarn add yup

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '../../assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string().email('Insira um email válido.').required('email obrigatório'),
+  password: Yup.string().required('senha obrigatória'),
+});
 
 export default function SignIn() {
   function handleSubmit(data) {
@@ -12,7 +18,7 @@ export default function SignIn() {
   return <>
     <img src={logo} alt="GoBarber" />
 
-    <Form onSubmit={handleSubmit}>
+    <Form schema={schema} onSubmit={handleSubmit}>
       <Input name="email" type="email" placeholder="Seu e-mail"  />
       <Input name="password" type="password" placeholder="Senha"  />
 
