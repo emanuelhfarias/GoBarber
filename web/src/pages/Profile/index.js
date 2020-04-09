40902a98070ed47fc5972617c5e5d7067f94ab95
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-04-08 22:10:19 -0300

    create Profile form

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu endereço de email" />
+
+        <hr />
+
+        <Input name="oldPassword" type="password" placeholder="Senha atual" />
+        <Input name="password" type="password" placeholder="Nova senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
