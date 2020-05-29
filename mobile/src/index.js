12a9e77c4d26ec341d9e9751157830d4ba8a8342
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-05-28 22:43:32 -0300

    install and configure react-navigation
    react-navigation changes a lot, so see the docs:
    reactnavigation.org/docs/en/getting-started.html
    
    yarn add react-navigation
    yarn add react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
    
    react-native start --reset-cache

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,28 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
+import Routes from './routes';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Hi!</Text>
-      </View>
-    </>
-  );
-};
-
-export default App;
+export default function App() {
+  return <Routes />;
+}
