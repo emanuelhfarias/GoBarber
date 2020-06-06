e7da9c47d4fcfcb89728703196fb6ea8be3b0d25
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-06-06 15:59:40 -0300

    fix lint

--- mobile/src/components/Appointment/styles.js
+++ mobile/src/components/Appointment/styles.js
@@ -2,9 +2,9 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   margin-bottom: 15px;
-  padding: 20px
+  padding: 20px;
   border-radius: 4px;
-  background: #FFF;
+  background: #fff;
 
   display: flex;
   flex-direction: row;
